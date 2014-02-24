f9a4e3de0ba2690bf72ebc4d9961669a0c7bdca9
MarkusWagner1994 <markwag1337@gmail.com>
2014-02-24 15:17:47 +0100

    daos for  user and address

---
+++ src/dto/Adress.java
@@ -0,0 +1,65 @@
+package dto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Markus
+ * Date: 05.02.14
+ * Time: 13:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Adress {
+    private int id, plz;
+    private String location, street, housNr;
+
+    public Adress(int id, int plz, String location, String street, String housNr) {
+        this.id = id;
+        this.plz = plz;
+        this.location = location;
+        this.street = street;
+        this.housNr = housNr;
+    }
+
+    public String getSQLString() {
+        return id + ", " + plz + ", '" + location + "', '" + street + "', '" + housNr + "'" ;
+    }
+
+    public String getHousNr() {
+        return housNr;
+    }
+
+    public void setHousNr(String housNr) {
+        this.housNr = housNr;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public void setPlz(int plz) {
+        this.plz = plz;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
--- src/dto/User.java
+++ src/dto/User.java
@@ -1,48 +1,95 @@
 package dto;
 
 /**
- * @author Markus
+ * Created with IntelliJ IDEA.
+ * User: Markus
+ * Date: 24.02.14
+ * Time: 14:26
+ * To change this template use File | Settings | File Templates.
  */
 public class User {
-    private String email, password;
+    private String email, salt, firstName, lastName, hash, telNr;
+    private int id, adressID;
 
-    public User(String[] loginData) {
-        email = loginData[0];
-        password = loginData[1];
-    }
-
-    public User(String email, String password) {
+    public User(String email, String salt, String firstName, String lastName, String hash, int id, String telNr, int adressID) {
         this.email = email;
-        this.password = password;
+        this.salt = salt;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hash = hash;
+        this.id = id;
+        this.telNr = telNr;
+        this.adressID = adressID;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-
-        this.email = email;
+    public String getSQLString() {
+        return id + ", '" + email + "', '" + salt + "', '" + hash + "', " +
+                "'" + firstName +  "', '" + lastName + "', '"+ telNr + "'," + adressID;
     }
 
     public String getEmail() {
-
         return email;
     }
 
-    public String getPassword() {
-        return password;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final User other = (User) obj;
-        return email.equals(other.email) && password.equals(other.password);
+
+    public String getSalt() {
+        return salt;
     }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTelNr() {
+        return telNr;
+    }
+
+    public void setTelNr(String telNr) {
+        this.telNr = telNr;
+    }
+
+    public int getAdressID() {
+        return adressID;
+    }
+
+    public void setAdressID(int adress) {
+        this.adressID = adress;
+    }
+
 }
